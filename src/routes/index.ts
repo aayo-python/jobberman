d63113ba0474af295f87b0fffa74c6b6e6d3fd7b
janto-pee <ayobami_adejumo@yahoo.com>
2025-07-16 16:09:04 -0700

    feat(users): Implement user registration endpoint - Create POST /api/users route. - Verify user email - Forgot password - Password reset endpoint .

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import UserRouter from "./user.routes";
 
 const router = express.Router();
 
 router.get("/healthcheck", (_, res) => {
   res.sendStatus(200);
 });
+
+/**
+ * INTERNAL
+ */
+router.use(UserRouter);
 
 export default router;
